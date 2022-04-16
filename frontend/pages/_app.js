a6bea7539a36799bd3b5d8b4c5a20c7e9dac8b53
Serena Yung <serenayung2005@gmail.com>
2022-04-16 19:02:41 -0400

    a

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,9 +2,42 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
+    <div className='bg-black'>
       <Component {...pageProps} />
-      <p>slkdfjalksdjfls</p>
+      <div className='flex justify-between w-full h-full text-white relative p-12'>
+        
+        <div className='flex justify-between flex-col w-1/2 h-full'>
+          <p className='text-center'>eduHUB</p>
+          <div className='flex flex-row justify-between w-full h-full'>
+            <img></img>
+            <img></img>
+            <img></img>
+            <img></img>
+
+          </div>
+        </div>
+
+        <div className='flex justify-between flex-col w-1/2'>
+          <p className='text-center'>Browse Subjects</p>
+          <div className='p-5 flex flex-col text-center'>
+            <a>Biology</a>
+            <a>beep</a>
+            <a>boop</a>
+            <a>beep</a>
+          </div>
+        </div>
+
+        <div className='flex justify-between flex-col w-1/2'>
+          <p className='text-center'>Contact</p>
+          <div className='p-5 flex flex-col text-center'>
+            <a>asdsadas</a>
+            <a>asdsadas</a>
+            <a>asdsadas</a>
+            
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -7,13 +7,15 @@ export default function Home() {
       <div className='h-screen block relative self-center mx-20'>
         <div className='h-screen mx-20 mt-auto flex flex-row-reverse justify-between relative self-center'>
           <img className='w-full h-1/2 rounded-2xl border-8 border-double border-black m-auto'></img>
-            <div className='m-auto px-16 py-16 font-mono'>
+            
+            <div className='m-auto px-16 py-16 font-mono p-12'>
               <h1 className= 'text-transparent text-5xl bg-clip-text bg-gradient-to-r from-light-pink to-pink-white'>eduHUB</h1>
               <p className='text-white'>A service dedicated to adskfjaskdlfjs</p>
               <a href='/home'>
                 <button className='absolute bg-pink-white mt-5 rounded-2xl pb-1'>Get Started</button>
               </a>
             </div>
+
         </div>
       </div>
     </div>
