852683daadbe52c3707cb59774dbd7769b124cb3
warren yun <wyun2006@gmail.com>
2022-04-17 00:42:43 -0400

    pain

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,10 +1,9 @@
 const Nav = () => {
     return (
-        <div className="relative w-1/2 py-8 text-white rounded-xl self-center m-auto t-12">
-            <div className="relative flex w-full justify-between font-mono">
-                <a href = '/' className="text-transparent bg-clip-text bg-gradient-to-r from-light-pink to-pink-white rounded-2xl p-2 hover:scale-105 duration-200">eduHUB</a>
-
-                <a className="self-end">asdfasdf</a>
+        <div className="relative self-center w-1/2 py-8 m-auto text-2xl text-white rounded-xl t-12">
+            <div className="relative flex justify-between w-full font-mono">
+                <a href = '/' className="p-2 text-transparent duration-200 bg-clip-text bg-gradient-to-r from-light-pink to-pink-white rounded-2xl hover:scale-105">eduHUB</a>
+                <a className="self-end hover:scale-105 hover:cursor-pointer" href="/create/upload">Create</a>
             </div>
             
         </div>
--- frontend/pages/account/register.js
+++ frontend/pages/account/register.js
@@ -8,13 +8,15 @@ export default function Register() {
     axios.post("http://localhost:4000/account/create", {
       name: name,
       email: email,
+      
     });
-    
   };
 
   return (
-    <div className="relative">
-      <h1 className="text-5xl font-light font-sans text-transparent bg-clip-text bg-gradient-to-r from-pink-600 to-light-pink p-2 text-center">Register</h1>
+    <div className="relative h-screen my-auto">
+      <h1 className="p-2 font-sans text-5xl font-light text-center text-transparent bg-clip-text bg-gradient-to-r from-pink-600 to-light-pink">
+        Register
+      </h1>
       <form
         className="flex flex-col items-center justify-center px-16 m-8"
         onSubmit={getData}
@@ -24,7 +26,7 @@ export default function Register() {
           name="email"
           type="text"
           autoComplete="off"
-          className="bg-white/30 text-deep-blue p-3 border-black rounded-xl m-2 hover:bg-white/40 focus:bg-white focus:w-2/5 transition-all"
+          className="p-3 m-2 transition-all border-black bg-white/30 text-deep-blue rounded-xl hover:bg-white/40 focus:bg-white focus:w-2/5"
           required
           placeholder="Email"
         />
@@ -33,13 +35,22 @@ export default function Register() {
           name="name"
           type="text"
           autoComplete="off"
-          className="bg-white/30 text-deep-blue p-3 border-black rounded-xl m-2 hover:bg-white/40 focus:bg-white focus:w-2/5 transition-all" 
+          className="p-3 m-2 transition-all border-black bg-white/30 text-deep-blue rounded-xl hover:bg-white/40 focus:bg-white focus:w-2/5"
           required
           placeholder="Name"
         />
+        <input
+          id="pw"
+          name="pw"
+          type="text"
+          autoComplete="off"
+          className="p-3 m-2 transition-all border-black bg-white/30 text-deep-blue rounded-xl hover:bg-white/40 focus:bg-white focus:w-2/5"
+          required
+          placeholder="Password"
+        />
         <button
           type="submit"
-          className="w-1/5 bg-light-pink/80 text-deep-blue p-3 border-black rounded-xl m-2 hover:bg-light-pink/100 transition-all"
+          className="w-1/5 p-3 m-2 transition-all border-black bg-light-pink/80 text-deep-blue rounded-xl hover:bg-light-pink/100"
         >
           Sign up
         </button>
