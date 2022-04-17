2d5be33b1918ba22eda61883fa49b046c92e6cca
warren yun <wyun2006@gmail.com>
2022-04-17 03:57:59 -0400

    funny navbar

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,28 +1,67 @@
 const Nav = () => {
-    return (
-        <div className="relative w-1/2 py-8 text-white rounded-xl self-center m-auto t-12">
-            <div className="relative flex w-full justify-between font-mono">
-                <a href = '/' className="text-transparent bg-clip-text bg-gradient-to-r from-light-pink to-pink-white rounded-2xl p-2 hover:scale-105 duration-200">eduHUB</a>
-                
-                
-                <div className="flex relative">
-                    <a className="self-center hover:scale-105 hover:cursor-pointer" href="/create/upload">Create</a>
-                    <a href='/account/register' className="self-center">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="h-6 pl-2 ml-2 self-center"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                        </svg>
-                    </a>
-                </div>
-            </div>
-            
+  return (
+    <div className="relative self-center w-1/2 py-8 m-auto text-white rounded-xl t-12">
+      <div className="relative flex justify-between w-full font-mono">
+        <a
+          href="/"
+          className="p-2 text-transparent duration-200 bg-clip-text bg-gradient-to-r from-light-pink to-pink-white rounded-2xl hover:scale-105"
+        >
+          eduHUB
+        </a>
+
+        <div className="relative flex">
+          <a href="/account/register" className="self-center mr-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-8 pl-2 ml-2 self-center"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </a>
+          <a href="/browse" className="self-center mr-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-8"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+              />
+            </svg>
+          </a>
+          <a href="/create/upload" className="self-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-8"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+              />
+            </svg>
+          </a>
         </div>
-    );
-  };
-  
-  export default Nav;
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
