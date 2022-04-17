d880df8f459c310076636bdff2a5cc8700d3d17b
Serena Yung <serenayung2005@gmail.com>
2022-04-17 09:25:52 -0400

    msg

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
           <img src='/sharpboi.png' className='animate-bop w-96 m-auto border-8 border-black border-double h-1/2 rounded-2xl'></img>
             <div className='px-16 py-16 m-auto font-mono'>
               <h1 className= 'text-5xl text-transparent bg-clip-text bg-gradient-to-r from-light-pink to-pink-white'>eduHUB</h1>
-              <p className='text-white'>A service dedicated to adskfjaskdlfjs</p>
+              <p className='text-white'>A service dedicated to allowing users to post their own educational content and as well as browse videos based on topics that they want to learn.</p>
               <a href='/browse'>
                 <button className='absolute p-2 mt-5 text-black duration-200 bg-pink-white rounded-2xl hover:scale-110'>Get Started</button>
               </a>
